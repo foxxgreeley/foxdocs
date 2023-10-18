59ba833588aab11f136360b6525e4b3107804bbf
Foxx Greeley <foxx.greeley@marsworks.com>
2023-10-18 10:11:07 -0400

    fix: error resolution for postcss

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,18 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
+				accent: '#ff0080',
+				gray: {
+					'100': '#f7fafc',
+					'200': '#edf2f7',
+					'300': '#e2e8f0',
+					'400': '#cbd5e0',
+					'500': '#a0aec0',
+					'600': '#718096',
+					'700': '#4a5568',
+					'800': '#2d3748',
+					'900': '#1a202c',
+				},
 			},
 			fontFamily: {
 				// Your preferred text font. Starlight uses a system font stack by default.
