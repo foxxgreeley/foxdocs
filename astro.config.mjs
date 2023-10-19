62cbc85cb790874d6bd73d7164b3ee818e80bc4d
Foxx <foxx.greeley@marsworks.com>
2023-10-19 17:07:17 -0400

    feat: custom colors, replaced components, test document, and other fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,39 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from "@astrojs/tailwind";
+import rehypeExternalLinks from 'rehype-external-links';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      }
+    }]]
+  },
   integrations: [starlight({
     title: 'FoxDocs',
-	logo: {
-		replacesTitle: true,
-		light: './src/assets/light-logo.png',
-		dark: './src/assets/dark-logo.png',
-		width: 50,
-	},
-	customCss: [
-		// Path to your Tailwind base styles:
-		'./src/tailwind.css',
-	],
-	components: {
-    // Override the default `Header` component.
-    Header: './src/components/Header.astro',
-    Hero: './src/components/Hero.astro',
-		SocialIcons: './src/components/SocialIcons.astro',
-		Pagination: './src/components/Pagination.astro',
-	},
+    logo: {
+      replacesTitle: true,
+      light: './src/assets/light-logo.png',
+      dark: './src/assets/dark-logo.png',
+      width: 50
+    },
+    customCss: [
+    // Path to your Tailwind base styles:
+    './src/tailwind.css'],
+    components: {
+      Header: './src/components/Header.astro',
+      Hero: './src/components/Hero.astro',
+      SocialIcons: './src/components/SocialIcons.astro',
+      Pagination: './src/components/Pagination.astro',
+      MarkdownContent: './src/components/MarkdownContent.astro',
+      ContentPanel: './src/components/ContentPanel.astro',
+    },
     social: {
       github: 'https://github.com/foxxgreeley'
     },
@@ -38,10 +49,8 @@ export default defineConfig({
         directory: 'tools'
       }
     }]
-  }), tailwind(
-	{
-		// Disable the default base styles:
-		applyBaseStyles: false,
-	}
-  )]
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false
+  }), vue()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,10 @@
 const starlightPlugin = require('@astrojs/starlight-tailwind');
 
-// Generated color palettes 
-const accent = { 200: '#acc9ff', 600: '#0453ff', 900: '#082a75', 950: '#0b204f' };
-const gray = { 100: '#f6f6f6', 200: '#eeeeee', 300: '#c2c2c2', 400: '#8b8b8b', 500: '#585858', 700: '#383838', 800: '#272727', 900: '#181818' };
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors: { accent, gray },
 			fontFamily: {
 				sans: [
 					'Inter',
