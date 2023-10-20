709b182be2f3c4514033d2e813bf6b10bced2764
Foxx <foxx.greeley@marsworks.com>
2023-10-20 01:18:24 -0400

    feat: rewrite of main theme, components, and intro docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,43 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from 'rehype-external-links';
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank',
       content: {
-        type: 'text',
-        value: ' 🔗'
-      }
+        type: 'element',
+        tagName: 'svg',
+        properties: {
+          viewBox: '0 0 24 24',
+          id: 'lucide-external-link',
+          width: 14,
+          height: 14,
+          fill: 'none',
+          stroke: 'currentColor',
+          'stroke-linecap': 'round',
+          'stroke-linejoin': 'round',
+          'stroke-width': 2,
+        },
+        children: [
+          {
+            type: 'element',
+            tagName: 'path',
+            properties: {
+              d: 'M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6m4-3h6v6m-11 5L21 3',
+            },
+          },
+        ],
+      },
     }]]
   },
   integrations: [starlight({
     title: 'FoxDocs',
     logo: {
-      replacesTitle: true,
+      replacesTitle: false,
       light: './src/assets/light-logo.png',
       dark: './src/assets/dark-logo.png',
       width: 50
@@ -32,23 +52,32 @@ export default defineConfig({
       SocialIcons: './src/components/SocialIcons.astro',
       Pagination: './src/components/Pagination.astro',
       MarkdownContent: './src/components/MarkdownContent.astro',
-      ContentPanel: './src/components/ContentPanel.astro',
+      PageTitle: './src/components/PageTitle.astro',
     },
     social: {
       github: 'https://github.com/foxxgreeley'
     },
     sidebar: [{
-      label: 'Guides',
+      label: 'Getting Started',
       autogenerate: {
-        directory: 'guides'
+        directory: 'getting-started'
       }
-    }, {
+    },
+    {
+      label: 'Laravel',
+      collapsed: false,
+      autogenerate: {
+        directory: 'laravel'
+      }
+    },
+    {
       label: 'Tools',
-      collapsed: true,
+      collapsed: false,
       autogenerate: {
         directory: 'tools'
       }
-    }]
+    }
+  ]
   }), tailwind({
     // Disable the default base styles:
     applyBaseStyles: false
---
+++ src/content/docs/getting-started/introduction.md
@@ -0,0 +1,18 @@
+---
+title: Introduction
+description: FoxDocs is a personal resource for documentation and guides relating to PHP, Laravel, and other web development topics.
+sidebar:
+  order: 1
+---
+
+## About the website
+Foxdocs is built using Astro, a static site generator for building lightning-fast websites with minimal JS. This site runs on the Starlight package - which is a custom theme by Astro.
+
+This site is built with TailwindCSS in mind and all content is written in Markdown. Vercel is used for hosting and deployment as this project is primarily a learning experience andn not intended for production use.
+
+
+### Mentions
+
+- [Astro](https://astro.build/) is the web framework used to build this site.
+- [TailwindCSS](https://tailwindcss.com/) is the CSS framework used to style this site.
+- [Vercel](https://vercel.com/) is the hosting platform used to deploy this site.
